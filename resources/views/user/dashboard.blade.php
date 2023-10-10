c98c6d381a55961ffc27efe23f0e6418b5e8945b
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-10-11 01:21:43 +0500

    referral link model added

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -9,6 +9,56 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>User | Dashboard</title>
 
+    <style>
+        /* The Modal (background) */
+        .modal {
+            display: none;
+            /* Hidden by default */
+            position: fixed;
+            /* Stay in place */
+            z-index: 1;
+            /* Sit on top */
+            padding-top: 100px;
+            /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%;
+            /* Full width */
+            height: 100%;
+            /* Full height */
+            overflow: auto;
+            /* Enable scroll if needed */
+            background-color: rgb(0, 0, 0);
+            /* Fallback color */
+            background-color: rgba(0, 0, 0, 0.4);
+            /* Black w/ opacity */
+        }
+
+        /* Modal Content */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 60px;
+            padding: 60px;
+            border: 1px solid #888;
+            width: 80%;
+        }
+
+        /* The Close Button */
+        .close {
+            color: #aaaaaa;
+            float: right;
+            font-size: 28px;
+            font-weight: bold;
+        }
+
+        .close:hover,
+        .close:focus {
+            color: #000;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    </style>
+
 </head>
 
 <body style="background-color: #eee !important;height: 740px;">
@@ -35,8 +85,8 @@
                         &nbsp;
                     </div>
                     <div class="column_box" style="float: left;padding-top: 5px;">
-                        <span style=""><b>Rs.{{ auth()->user()->balance }}</b></span> <i class="fa fa-arrow-circle-o-right"
-                            aria-hidden="true" style="color: #000 !important;"></i>
+                        <span style=""><b>Rs.{{ auth()->user()->balance }}</b></span> <i
+                            class="fa fa-arrow-circle-o-right" aria-hidden="true" style="color: #000 !important;"></i>
                     </div>
                     <div class="column_box" style="float: right;text-align: right;padding-top: 5px;">&nbsp;</div>
                     <div class="column_box" style="float: left;padding-top: 5px;">
@@ -69,13 +119,43 @@
                         <h5 style="font-size: 11px;">Dashboard</h5>
                     </div>
                 </div>
-                <div class="column">
+                <div class="column" id="myBtn">
                     <div class="inner_data" style="background-color: #fff;border-radius: 10px;padding:20px;">
                         <i class="fa fa-user-plus" style="font-size: 30px !important;" aria-hidden="true"></i>
                         <h5 style="font-size: 11px;">Invite & Earn</h5>
                     </div>
                 </div>
             </div>
+        </div>
+
+        {{-- model --}}
+
+        <div id="myModal" class="modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <div class="my-5">
+                    <input type="text" style="padding: 15px;width:90%;border:1px solid black;"
+                        value="{{ route('register', ['referal' => Auth::user()->email]) }}" id="myInput" readonly>
+                    <div class="">
+                        <button onclick="copy()"
+                            style="padding: 16px 25px;margin-top:10px;border:none;border-radius:12px;background-color:green;color:white">copy</button>
+                    </div>
+                    <script>
+                        function copy() {
+                            // Get the text field
+                            var copyText = document.getElementById("myInput");
+                            copyText.select();
+                            copyText.setSelectionRange(0, 99999);
+                            navigator.clipboard.writeText(copyText.value);
+                            // Alert the copied text
+                            alert("Copied the text: " + copyText.value);
+                        }
+                    </script>
+                </div>
+            </div>
+
         </div>
 
         <div class="wrapper_three" style="border: 0px solid green;padding: 0px 15px;">
@@ -246,6 +326,37 @@
         </div>
     </div>
     </div>
+
+    <footer>
+        <script>
+            // Get the modal
+            var modal = document.getElementById("myModal");
+
+            // Get the button that opens the modal
+            var btn = document.getElementById("myBtn");
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks the button, open the modal
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        </script>
+    </footer>
+
     <script src="{{ asset('assets/js/slider.js') }}"></script>
 </body>
 
