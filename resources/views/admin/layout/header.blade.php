d363ee4335d5d6b7509d696f002fecce7f9702e4
raheel0316 <raheelanwaar0316@gmail.com>
2023-10-17 14:25:40 -0700

    added

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -281,7 +281,7 @@
                     <li>
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button class="btn btn-danger">Logout</button>
+                            <button type="submit" class="btn btn-danger">Logout</button>
                         </form>
                     </li>
                 </ul>
