d7cc9ec295ad692e2c34e25649dc733d5369d638
raheel0316 <raheelanwaar0316@gmail.com>
2023-10-19 11:46:37 -0700

    level added in user system

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('mobile');
             $table->string('package');
-            $table->decimal('balance',10,3)->default(0);
+            $table->string('level')->default('level 0');
+            $table->decimal('balance', 10, 3)->default(0);
             $table->string('referral')->default('default');
             $table->string('status')->default('pending');
             $table->string('email')->unique();
